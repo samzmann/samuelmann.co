a59092814d86b36a089bcae2170145560c766581
LaVielle <samuelcmann@gmail.com>
2019-01-26 13:50:31 +0100

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,28 @@ class App extends Component {
   componentDidMount(){
 
     getAllMessages()
-      .then(res => this.setState({ messages: res }))
-      .catch(err => console.log(err))
+      .then(res => {
+        const newMsg = {
+          type: 'system',
+          msg: "welcome, let's chat",
+          timestamp: Date.now()
+        }
+        this.addNewMessage(newMsg)
+          .then(messages => {
+            console.log(messages);
+            console.log(res);
+            this.setState({ messages: [...this.state.messages, ...res] })
+          })
+      })
+      .catch(err => {
+        console.log(err)
+        const newMsg = {
+          type: 'system',
+          msg: 'server error, please reload page (ಥ﹏ಥ)',
+          timestamp: Date.now()
+        }
+        this.addNewMessage(newMsg)
+      })
 
   }
 
@@ -24,34 +44,30 @@ class App extends Component {
 
       const d = new Date(msg.timestamp)
       const dM1 = messages[index-1] ? new Date(messages[index-1].timestamp) : null
-      const showDateInfo = !dM1 || ((d - dM1) > 60000)
+      const showDateInfo = !dM1 || ((d - dM1) > 60000) || dM1 > d
 
-      return (
-        <div key={msg._id}>
-          {showDateInfo &&
-            <div className="App-line">{`${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}`}</div>
-          }
-          <div className={`App-line ${msg.type === 'user' ? "App-usrmsg" : "App-sysmsg"}`}>{msg.msg}</div>
-        </div>
-      )
+      return <div key={msg._id} className={`App-line ${msg.type === 'user' ? "App-usrmsg" : "App-sysmsg"}`}>{showDateInfo ? `[${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}]` : ''} {msg.msg}</div>
     })
   }
 
   addNewMessage = (msg) => {
-    const { messages } = this.state
-    const newMsg = {
-      _id: messages.length,
-      type: msg.type,
-      msg: msg.msg,
-      timestamp: msg.timestamp
-    }
-    this.setState({ messages: [...this.state.messages, newMsg] })
+    return new Promise((resolve, reject) => {
+      const { messages } = this.state
+      const newMsg = {
+        _id: messages.length,
+        type: msg.type,
+        msg: msg.msg,
+        timestamp: msg.timestamp
+      }
+      this.setState({ messages: [...this.state.messages, newMsg] }, () => {
+        resolve(this.state.messages)
+      })
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <div className="App-line App-sysmsg">welcome, let's chat</div>
         {this.renderMessages()}
         <MsgForm addNewMessage={this.addNewMessage}/>
       </div>
